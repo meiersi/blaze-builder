566271db73d4875042eda28c24292b5084ce5db2
Simon Meier <iridcode@gmail.com>
2010-10-17 18:32:41 +0200

    refactored scaling comparison infrastructure

--- Criterion/ScalingBenchmark.hs
+++ Criterion/ScalingBenchmark.hs
@@ -17,8 +17,11 @@ module Criterion.ScalingBenchmark where
 
 import Prelude hiding (lines)
 
-import Data.List (unfoldr)
+import Data.Function (on)
+import Data.List (unfoldr, transpose, sortBy)
 import Data.Word (Word8)
+
+import qualified Data.Vector.Generic as V
 
 import Text.Blaze.Builder
 
@@ -36,6 +39,7 @@ import Criterion.Environment
 import Criterion.Monad
 import Criterion.Types
 import Criterion.Config
+import Criterion.Measurement (secs)
 
 import Control.Arrow (first, second)
 import Control.Monad
@@ -44,6 +48,7 @@ import Control.Monad.Trans.Reader
 
 import Statistics.Types
 import Statistics.Sample
+import Statistics.Quantile (continuousBy, medianUnbiased)
 
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
@@ -57,8 +62,6 @@ randomWord8s :: [Word8]
 randomWord8s = map fromIntegral $ unfoldr (Just . R.next) (R.mkStdGen 666) 
 -}
 
-mapDataPoints f (ScalingSample info samples) = ScalingSample info (map (first f) samples)
-
 -- | An infinite list of 'Word8'.
 word8s :: [Word8]
 word8s = cycle [0..]
@@ -70,20 +73,23 @@ main :: IO ()
 main = do
     let config = defaultConfig
     env <- withConfig config measureEnvironment
-    samples <- withConfig config (runScalingBenchmarks env benchs testPoints)
+    samples <- withConfig config (runScalingComparison env comparison)
+    return ()
+    {-
     let samples' :: [ScalingSample String Double Sample]
         samples' = map (mapDataPoints fromIntegral) samples
         renderable = renderScalingMeans "Packing [Word8]" "bytes" samples'
     renderableToWindow renderable 640 480
+    -}
   where
-    testPoints :: [Int]
-    testPoints = map (2^) [0..22]
-    benchs =
-      [ ScalingBenchmark "S.pack"     (\x-> whnf S.pack                (take x word8s))
-      , ScalingBenchmark "L.pack"     (\x-> whnf (L.length . L.pack)   (take x word8s))
-      , ScalingBenchmark "packStrict" (\x-> whnf packStrict            (take x word8s))
-      , ScalingBenchmark "packLazy"   (\x-> whnf (L.length . packLazy) (take x word8s))
-      ]
+    vs :: [Int]
+    vs = map (2^) [0..2]
+    comparison = compareBenchmarks "Packing [Word8]" "bytes" vs $
+        [ ScalingBenchmark "S.pack"     (\x-> whnf S.pack                (take x word8s))
+        , ScalingBenchmark "L.pack"     (\x-> whnf (L.length . L.pack)   (take x word8s))
+        , ScalingBenchmark "packStrict" (\x-> whnf packStrict            (take x word8s))
+        , ScalingBenchmark "packLazy"   (\x-> whnf (L.length . packLazy) (take x word8s))
+        ]
 
 packLazy :: [Word8] -> L.ByteString
 packLazy = toLazyByteString . fromWord8s
@@ -98,34 +104,136 @@ packStrict = toByteString . fromWord8s
 
 -- | A scaling benchmark denotes a named benchmark constructor, which can be
 -- used to investigate the scaling behaviour of a 'Benchmarkable' function.
-data ScalingBenchmark i a where
+data ScalingBenchmark a where
     ScalingBenchmark :: (Benchmarkable b)
-                     => i                   -- ^ Additional benchmark information
+                     => String              -- ^ Benchmark name
                      -> (a -> b)            -- ^ Benchmark constructor
-                     -> ScalingBenchmark i a  -- ^ Scaling benchmark
+                     -> ScalingBenchmark a  -- ^ Scaling benchmark
+
+-- | A comparison of several benchmarks on a common set of test points.
+data ScalingComparison a = ScalingComparison
+      { scName         :: String
+      , scTestUnit     :: String
+      , scTestValues   :: [a]                   -- INV: non-empty
+      , scBenchmarks   :: [ScalingBenchmark a]  -- INV: non-empty
+      , scMeasurements :: [[Sample]]      -- measurements: benchmarks * test points;
+                                          -- [] iff the benchmarks haven't been run yet
+      }
+
+-- | Compare several benchmarks on a set of test values.
+compareBenchmarks :: String               -- ^ Name of comparison.
+                  -> String               -- ^ Unit of test values.
+                  -> [a]                  -- ^ Test values.
+                  -> [ScalingBenchmark a] -- ^ Benchmarks to compare.
+                  -> ScalingComparison a  -- ^ Resulting scaling comparison.
+compareBenchmarks name unit vs bs = 
+    ScalingComparison name unit vs bs []
+
+-- | Annotate the measurements stored in a 'ScalingComparison'.
+annotateMeasurements :: ScalingComparison a 
+                     -> [(ScalingBenchmark a, [(a,Sample)])]
+annotateMeasurements sc =
+    zip (scBenchmarks sc) (map (zip (scTestValues sc)) (scMeasurements sc))
+
+-- | Run a 'ScalingComparison'.
+runScalingComparison :: Show a
+                     => Environment                -- ^ Criterion environment.
+                     -> ScalingComparison a        -- ^ Comparison to run (may or may not contain any measurements already).
+                     -> Criterion (ScalingComparison a) -- ^ Comparison with 'scMeasurements' field set to new measurements.
+runScalingComparison env sc = do
+    samples <- mapM runBenchmarks $ scTestValues sc
+    return $ sc { scMeasurements = transpose samples }
+  where
+
+    runBenchmarks x = do
+        let testPointStr = show x ++ " " ++ scTestUnit sc
+        liftIO $ putStrLn $ ""
+        liftIO $ putStrLn $ "running benchmarks for " ++ testPointStr
+        samples <- sequence 
+            [ ((,) name) `liftM` runBenchmark env (mkBench x) 
+            | ScalingBenchmark name mkBench <- scBenchmarks sc ]
+        liftIO $ putStrLn $ ""
+        liftIO $ putStrLn $ "ranking for " ++ testPointStr
+        liftIO $ quickAnalysis samples
+        return $ map snd samples
+
+    quickAnalysis samples = do
+        let indent = length (show $ length samples + 1) + 2
+            extent = maximum (map (length . fst) samples) + 2
+        mapM (printStatistics indent extent) 
+             (zip [1..] $ sortBy (compare `on` (mean . snd)) samples)
+ 
+    printStatistics :: Int -> Int -> (Int, (String, Sample)) -> IO ()
+    printStatistics indent extent (i, (info, sample)) = putStrLn $
+        rightAlign indent (show i) ++ ". " ++
+        leftAlign extent (info ++ ":") ++
+        "mean " ++ secs (mean sample) ++ 
+        " (2p " ++ secs p2 ++ 
+        ", 98p " ++ secs p98 ++
+        ")"
+      where
+        -- percentiles
+        p2  = continuousBy medianUnbiased  2 100 sample
+        p98 = continuousBy medianUnbiased 98 100 sample
+
+    rightAlign n cs = take (n - length cs) (repeat ' ') ++ cs
+    leftAlign  n cs = cs ++ take (n - length cs) (repeat ' ')
+
+renderScalingComparison :: ScalingComparison a -> Renderable ()
+renderScalingComparison = undefined
+
+{-
 
 data ScalingSample i a s where
     ScalingSample :: i                -- ^ Additional benchmark information
-                  -> [(a,s)]          -- ^ Test points annotated with measured sample
+                  -> TestPoints a     -- ^ Test points used for this sample
+                  -> [s]              -- ^ Measurements for every test point
                   -> ScalingSample i a s -- ^ Scaling sample
 
+
+{-
 -- | Run a 'ScalingBenchmark'.
 runScalingBenchmark :: Environment                   -- ^ Criterion environment
                     -> ScalingBenchmark i a          -- ^ Scaling benchmark
-                    -> [a]                           -- ^ Test points to run benchmark on
+                    -> TestPoints a                  -- ^ Test points to run benchmark on
                     -> Criterion (ScalingSample i a Sample) -- ^ Measured scaling sample
-runScalingBenchmark env (ScalingBenchmark info mkBench) xs = do
+runScalingBenchmark env (ScalingBenchmark info mkBench) tp = do
     samples <- mapM (runBenchmark env . mkBench) xs
     return $ ScalingSample info (zip xs samples)
+-}
 
 
 -- | Run a series of 'ScalingBenchmark's on the same test points.
 runScalingBenchmarks :: Environment                   -- ^ Criterion environment
-                     -> [ScalingBenchmark i a]        -- ^ Scaling benchmarks
-                     -> [a]                           -- ^ Test points to run benchmark on
-                     -> Criterion [ScalingSample i a Sample] -- ^ Measured scaling samples
-runScalingBenchmarks env bs xs = 
-    sequence [runScalingBenchmark env b xs | b <- bs]
+                     -> [ScalingBenchmark String a]        -- ^ Scaling benchmarks
+                     -> TestPoints a                  -- ^ Test points to run benchmark on
+                     -> Criterion [ScalingSample String a Sample] -- ^ Measured scaling samples
+runScalingBenchmarks env bs tp = do
+      samples <- transpose `liftM` mapM runBenchmarks (testPoints tp)
+      return $ zipWith3 ScalingSample (map 
+    where
+      runBenchmarks x = do
+          liftIO $ putStrLn $ "running benchmarks for " ++ show x ++ " " ++ testUnit tp
+        samples <- sequence 
+            [ ((,) info) `liftM` runBenchmark env (mkBench x) 
+            | ScalingBenchmark info mkBench <- bs ]
+        quickAnalysis samples
+        return $ map snd samples
+
+    quickAnalysis samples = do
+        let indent = length (show $ length samples + 1) + 2
+        mapM (printStatistics indent) 
+             (zipWith [1..] $ sortBy (compare `on` (mean . snd)) samples)
+
+    printStatistics indent (i, (info, sample)) = liftIO $ putStrLn $
+        rightAlign indent (show i) ++ ". " ++
+        info ++ ": " ++
+        show (mean sample) ++ "s"
+        
+
+rightAlign :: Int -> String -> String
+rightAlign n cs = take (n - length cs) (replicate ' ') ++ cs
+
 
 -- Transforming scaling samples
 -------------------------------
@@ -315,5 +423,6 @@ measureSerializer (info, tests) = do
     []     -> return Nothing
     points -> return $ Just (info, points)
 
+-}
 -}
 -}
