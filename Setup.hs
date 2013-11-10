d2dd3c34b133b0145dfe2638eafcd6ca88fa5275
Leon P Smith <leon@melding-monads.com>
2013-11-10 14:31:08 -0500

    Fix Setup.hs to not preempt error messages
    For example,  if dependencies are missing, the old Setup.hs would
    return "error detecting bytestring version" instead of the correct
    "missing text".

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,6 @@
 import Distribution.Simple
 import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
 import Distribution.PackageDescription
 import Distribution.Version
 
@@ -9,6 +10,7 @@ import Distribution.Version
 
 main = defaultMainWithHooks simpleUserHooks {
     confHook = \pkg flags -> do
+        lbi <- confHook simpleUserHooks pkg flags
         let bytestring_version =
                 case [ versionBranch v
                      | (Dependency pkg ver)  <- configConstraints flags
@@ -16,7 +18,7 @@ main = defaultMainWithHooks simpleUserHooks {
                      , (Just v) <- [isSpecificVersion ver] ]
                   of
                      [v] -> v
-                     _   -> error "error detecting bytestring version"
+                     vs   -> error ("error detecting bytestring version  "  ++ show vs)
 
         let has_itoa_c  = ( FlagName "bytestring_has_itoa_c"
                           , bytestring_version >= [0,10]      )
@@ -27,9 +29,13 @@ main = defaultMainWithHooks simpleUserHooks {
         let update fs gs =
                  fs ++ [ g | g <- gs, not $ any (\f -> fst f == fst g) fs]
 
+        let flags  = configFlags lbi
+
         let flags' = flags { configConfigurationsFlags =
                                 update [has_itoa_c, has_builder]
                                        (configConfigurationsFlags flags) }
 
-        confHook simpleUserHooks pkg flags'
+        let lbi' = lbi { configFlags = flags' }
+        
+        return lbi'
   }
