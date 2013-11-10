be71d92b2efdfa36e36deeed9a83219a6f58e25a
Leon P Smith <leon@melding-monads.com>
2013-11-10 06:28:41 -0500

    Fix conditional compilation based on bytestring version

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,35 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.PackageDescription
+import Distribution.Version
+
+-- This checks the direct dependency of the bytestring package being
+-- compiled against to set the bytestring_has_itoa_c and
+-- bytestring_has_builder flags accordingly.
+
+main = defaultMainWithHooks simpleUserHooks {
+    confHook = \pkg flags -> do
+        let bytestring_version =
+                case [ versionBranch v
+                     | (Dependency pkg ver)  <- configConstraints flags
+                     , pkg == PackageName "bytestring"
+                     , (Just v) <- [isSpecificVersion ver] ]
+                  of
+                     [v] -> v
+                     _   -> error "error detecting bytestring version"
+
+        let has_itoa_c  = ( FlagName "bytestring_has_itoa_c"
+                          , bytestring_version >= [0,10]      )
+
+        let has_builder = ( FlagName "bytestring_has_builder"
+                          , bytestring_version >= [0,10,4]   )
+
+        let update fs gs =
+                 fs ++ [ g | g <- gs, not $ any (\f -> fst f == fst g) fs]
+
+        let flags' = flags { configConfigurationsFlags =
+                                update [has_itoa_c, has_builder]
+                                       (configConfigurationsFlags flags) }
+
+        confHook simpleUserHooks pkg flags'
+  }
